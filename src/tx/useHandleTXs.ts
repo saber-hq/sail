bf3a13686bbe7156ae7fca46cad7c66d034c1dd7
Ian Macalinao <ian@saber.so>
2021-08-30 13:57:06 -0500

    Capture SolanaTransactionError only

--- src/tx/useHandleTXs.ts
+++ src/tx/useHandleTXs.ts
@@ -43,7 +43,7 @@ export interface UseHandleTXsArgs {
   /**
    * Called whenever a transaction throws an error.
    */
-  onTxError?: (err: Error) => void;
+  onTxError?: (err: SolanaTransactionError) => void;
 }
 
 export interface UseHandleTXs {
