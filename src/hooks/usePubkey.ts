0f4878e1a33bb6ec25524883f3e66f02376fabee
Ian Macalinao <ian@saber.so>
2022-02-09 13:55:19 -0800

    useMemoPubkey

--- src/hooks/usePubkey.ts
+++ src/hooks/usePubkey.ts
@@ -26,3 +26,18 @@ export const usePubkey = (
     }
   }, [raw]);
 };
+
+/**
+ * Uses a {@link PublicKey}, ensuring the reference does not change if the key changes.
+ * @param raw The string or Pubkey representation of the key.
+ * @returns
+ */
+export const useMemoPubkey = (
+  raw: PublicKey | string | null | undefined
+): PublicKey | null | undefined => {
+  const keyNoMemo = usePubkey(raw);
+  return useMemo(() => {
+    return keyNoMemo;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [keyNoMemo?.toString()]);
+};
